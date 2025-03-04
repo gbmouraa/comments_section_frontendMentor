da280f676715f57dfde35ac961abb718286f3a8a
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2025-03-04 19:54:28 -0300

    refactor: changed the structure of the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
+import { useApp } from "./hooks/useApp";
 import { SwitchTheme } from "./components/switch-theme";
 import { Profiles } from "./components/profiles";
-import { AppProvider } from "./contexts/providers/app-provider";
 import { Posts } from "./components/posts-list";
+import { Loader } from "./components/loader";
 
 export const App: React.FC = () => {
+  const { isLoading } = useApp();
+
   return (
     <>
-      <AppProvider>
-        <div className="min-h-screen w-full bg-neutral-100 p-4 dark:bg-zinc-800 md:py-10">
-          <div className="flex flex-wrap items-center justify-between gap-6 md:flex-nowrap md:items-start">
-            <div className="order-0 min-w-[118px] md:-translate-y-5">
-              <SwitchTheme />
-            </div>
-            <div className="order-2 mx-auto">
-              <Posts />
-            </div>
-            <div className="order-1 md:order-2 md:-translate-y-5">
-              <Profiles />
-            </div>
-          </div>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className="min-h-screen w-full bg-neutral-100 p-4 dark:bg-zinc-800">
+          <SwitchTheme />
+          <Posts />
+          <Profiles />
         </div>
-      </AppProvider>
+      )}
     </>
   );
 };

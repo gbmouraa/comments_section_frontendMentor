850c36565242f86aebf5158e1cf5b609d5a9c7d5
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2025-03-05 18:59:50 -0300

    rename: comment-voting-button to voting-button

--- src/components/voting-button.tsx
+++ src/components/voting-button.tsx
@@ -1,17 +1,20 @@
 import { useState } from "react";
 import { Button } from "./ui/button";
 import { Plus, Minus } from "lucide-react";
+import { useApp } from "@/hooks/useApp";
 
-interface CommentVotingButtonProps {
+interface VotingButtonProps {
   score: number;
+  user: string;
 }
 
-export const CommentVotingButton: React.FC<CommentVotingButtonProps> = ({
+export const VotingButton: React.FC<VotingButtonProps> = ({
   score,
-}: CommentVotingButtonProps) => {
+  user,
+}: VotingButtonProps) => {
   const [votes, setVotes] = useState<number>(score);
+  const { storedApp, setStoredApp } = useApp();
 
-  // increment e decrement irão receber os dados do usuario antes do voto ser feito
   const incrementVote = () => {
     setVotes((prev) => prev + 1);
   };
@@ -21,11 +24,14 @@ export const CommentVotingButton: React.FC<CommentVotingButtonProps> = ({
   };
 
   return (
-    <div className="flex items-center overflow-hidden rounded-xl bg-neutral-100 dark:bg-zinc-800 md:absolute md:top-5 md:flex-col">
+    <div
+      className={`flex items-center overflow-hidden rounded-xl bg-neutral-100 dark:bg-zinc-800 md:absolute md:top-5 md:flex-col ${storedApp?.currentUser?.username === user && "cursor-not-allowed"}`}
+    >
       <Button
         variant="ghost"
         className="w-full px-3 py-5 text-indigo-300 shadow-none hover:bg-transparent hover:text-indigo-500"
         onClick={incrementVote}
+        disabled={storedApp?.currentUser?.username === user}
       >
         <Plus />
       </Button>
@@ -34,6 +40,7 @@ export const CommentVotingButton: React.FC<CommentVotingButtonProps> = ({
         variant="ghost"
         className="w-full px-3 py-5 text-indigo-300 hover:bg-transparent hover:text-indigo-500"
         onClick={decrementVote}
+        disabled={storedApp?.currentUser?.username === user}
       >
         <Minus />
       </Button>

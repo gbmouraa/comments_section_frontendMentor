5d402f2ab5146bfbf11a16b3359d2698f4e28423
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2025-03-09 12:04:15 -0300

    refactor: improved condition for rendering posts

--- src/components/posts-list.tsx
+++ src/components/posts-list.tsx
@@ -8,28 +8,29 @@ export const Posts: React.FC = () => {
   return (
     <div className="mt-10 flex -translate-y-5 flex-col items-center md:mt-6">
       <ul>
-        {storedApp?.posts.map((item, idx) => {
-          return (
-            <motion.li
-              key={item.id}
-              initial={{ opacity: 0, y: 20, scale: 0 }}
-              animate={{ opacity: 1, y: 0, scale: 1 }}
-              transition={{
-                delay: 0.4 + 0.2 * idx,
-                ease: [0.25, 0.46, 0.45, 0.94],
-                duration: 1,
-              }}
-            >
-              <Post
-                content={item.content}
-                id={item.id}
-                user={item.user}
-                replies={item.replies}
-                score={item.score}
-              />
-            </motion.li>
-          );
-        })}
+        {storedApp?.posts &&
+          storedApp.posts.map((item, idx) => {
+            return (
+              <motion.li
+                key={item.id}
+                initial={{ opacity: 0, y: 20, scale: 0 }}
+                animate={{ opacity: 1, y: 0, scale: 1 }}
+                transition={{
+                  delay: 0.4 + 0.2 * idx,
+                  ease: [0.25, 0.46, 0.45, 0.94],
+                  duration: 1,
+                }}
+              >
+                <Post
+                  content={item.content}
+                  id={item.id}
+                  user={item.user}
+                  replies={item.replies}
+                  score={item.score}
+                />
+              </motion.li>
+            );
+          })}
       </ul>
     </div>
   );
